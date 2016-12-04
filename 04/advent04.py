6c49ea473f974d6409ed461d39a74a9fe5e2a71d
iamevn <iamevn@users.noreply.github.com>
2016-12-03 21:47:52 -0800

    so it kinda sorta works but it's broken and there's a bug that I got lucky on my input with

---
+++ 04/advent04.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+from sys import argv,exit
+
+input_filename = "input"
+if len(argv) > 1:
+    input_filename = argv[1]
+
+try:
+    input = open(input_filename)
+except OSError:
+    exit("Cannot open file \"{}\"".format(input_filename))
+
+sum = 0
+
+def matches(letters, cksum):
+    #if any letters not in cksum are greater than those in cksum, false
+    #else true
+    # print(letters)
+    # print(cksum)
+    for c in cksum:
+        if not c in letters:
+            return False
+    for l in letters:
+        if not l in cksum:
+            for c in cksum:
+                if letters[l] > letters[c]:
+                    return False
+    return True
+
+for line in input:
+    words = line.split("-")
+    end = words[-1]
+    number, cksum = end.split("[")
+    number = int(number)
+    cksum = cksum[0:-2]
+    words = words[0:-1]
+    lettercounts = {}
+    for word in words:
+        for c in word:
+            if c in lettercounts:
+                lettercounts[c] += 1
+            else:
+                lettercounts[c] = 1
+    if matches(lettercounts, cksum):
+        sum += number
+
+
+# print(sum)
+# part 2
+
+input.seek(0)
+
+def incChar(ch, n):
+    new = ord(ch) + (n % 26)
+    if new > ord('z'):
+        new -= ord('a')
+    return chr(new)
+
+def decode(words, number):
+    line = ""
+    for word in words:
+        str = ""
+        for char in word:
+            str = str + incChar(char, number)
+        line = line + str + " "
+    print("{} : {}".format(line, number))
+        
+
+
+for line in input:
+    words = line.split("-")
+    end = words[-1]
+    number, cksum = end.split("[")
+    number = int(number)
+    cksum = cksum[0:-2]
+    words = words[0:-1]
+    lettercounts = {}
+    for word in words:
+        for c in word:
+            if c in lettercounts:
+                lettercounts[c] += 1
+            else:
+                lettercounts[c] = 1
+    if matches(lettercounts, cksum):
+        decode(words, number)
