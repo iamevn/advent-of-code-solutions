bc277c01a4f15af60f72b99fec8a03c22a6db7f8
iamevn <iamevn@users.noreply.github.com>
2016-12-04 22:35:34 -0800

    use hexdigest instead of digest

--- 05/advent05.py
+++ 05/advent05.py
@@ -9,25 +9,20 @@ password = ""
 print("________\r", end='')
 
 def check(i):
-    m = md5(doorid + str(i).encode()).digest()
-    if m[0] & b'\xFF'[0] == 0 \
-    and m[1] & b'\xFF'[0] == 0\
-    and m[2] & b'\xF0'[0] == 0:
-        return True, toChar(m[2] & b'\x0F'[0])
-    else:
-        return False, False
-
-def toChar(i):
-    return hex(i)[-1]
+    m = md5(doorid + str(i).encode()).hexdigest()
+    for c in m[0:5]:
+        if c != '0':
+            return False
+    return m[5]
 
 i = 0
 while len(password) < 8:
-    found, g = check(i)
+    found = check(i)
     if found:
-        print("{}".format(g), end='')
+        print(found, end='')
         sys.stdout.flush()
 
-        password = password + g
+        password = password + found
     i += 1
 
 print('')
@@ -35,13 +30,11 @@ print('')
 # part 2
 
 def check2(i):
-    m = md5(doorid + str(i).encode()).digest()
-    if m[0] & b'\xFF'[0] == 0 \
-    and m[1] & b'\xFF'[0] == 0\
-    and m[2] & b'\xF0'[0] == 0:
-        return True, int(m[2] & b'\x0F'[0]), toChar(m[3] >> 4)
-    else:
-        return False, False, False
+    m = md5(doorid + str(i).encode()).hexdigest()
+    for c in m[0:5]:
+        if c != '0':
+            return False, False, False
+    return True, int(m[5], base=16), m[6]
 
 password = "________\r"
 print(password, end='')
@@ -49,9 +42,8 @@ print(password, end='')
 i = 0
 while '_' in password:
     found, index, char = check2(i)
-    if found \
-    and index >= 0 \
-    and index < 8 \
+    if  found \
+    and index in range(8) \
     and password[index] == '_':
         password = password[:index] + char + password[index+1:]
         print(password, end='')
