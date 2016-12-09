bab83252473028d08987047187b5ee7403f245cd
iamevn <iamevn@users.noreply.github.com>
2016-12-08 22:28:32 -0800

    day 9 part 2 solved. (lol recursion limit)

--- 09/advent09.py
+++ 09/advent09.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from sys import argv,exit
+from sys import argv,exit,setrecursionlimit
 
 input_filename = "input"
 if len(argv) > 1:
@@ -62,11 +62,53 @@ for line in input:
                 marker = False
 
 
-print(len(out) - 1)
-print(length - 1)
-print(out)
+print(length - 1) # minus the newline
 
 
 # part 2
 input.seek(0)
 
+line = input.readline()[:-1] #chop off '\n'
+linelen = len(line)
+outlen = 0
+
+setrecursionlimit(999999999)
+# recursively find length of a section of line
+def sectionLength(start, length):
+    secLen = 0
+    ii = start
+    while ii < length + start:
+        # secLen += 1
+        # ii += 1
+        if line[ii] == '(':
+            jj = ii
+            while line[jj] != ')':
+                jj += 1
+            repLen, repCount = line[ii+1:jj].split('x')
+            repLen = int(repLen)
+            repCount = int(repCount)
+            secLen += sectionLength(jj+1, repLen) * repCount
+            ii = jj + repLen + 1
+        else:
+            secLen += 1
+            ii += 1
+
+    return secLen
+
+i = 0
+while i < linelen:
+    if line[i] == '(':
+        j = i
+        while line[j] != ')':
+            j += 1
+        repLen, repCount = line[i+1:j].split('x')
+        repLen = int(repLen)
+        repCount = int(repCount)
+        addedLen = sectionLength(j+1, repLen) * repCount 
+        outlen += addedLen
+        i = j + repLen + 1
+    else:
+        outlen += 1
+        i += 1
+
+print(outlen)
