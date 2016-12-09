8694289686ab6f495e692a718e1b3a6d56cbfc1f
iamevn <iamevn@users.noreply.github.com>
2016-12-08 22:39:03 -0800

    there's a more sane recursion limit. I tried @functools.lru_cache() but didn't notice any major speedup. redid part 1 to use part 2's logic

--- 09/advent09.py
+++ 09/advent09.py
@@ -11,75 +11,39 @@ try:
 except OSError:
     exit("Cannot open file \"{}\"".format(input_filename))
 
-out = ""
-length = 0
-
-inParen = False
-firstArg = False
-count = ""
-repeats = ""
-subject = ""
-marker = False
-for line in input:
-    for c in line:
-        if not marker:
-            if not inParen:
-                if c == '(':
-                    inParen = True
-                    firstArg = True
-                else:
-                    out += c
-                    length += 1
-            else:
-                if firstArg:
-                    if not c == 'x':
-                        count += c
-                    else:
-                        count = int(count)
-                        firstArg = False
-                else:
-                    if not c == ')':
-                        repeats += c
-                    else:
-                        repeats = int(repeats)
-                        inParen = False
-                        marker = True
-                        subject = ""
-                        length += count * repeats
-        else:
-            if count > 0:
-                subject += c
-                count -= 1
-            if count == 0:
-                while repeats > 0:
-                    out += subject
-                    repeats -= 1
-                inParen = False
-                firstArg = False
-                count = ""
-                repeats = ""
-                subject = ""
-                marker = False
-
-
-print(length - 1) # minus the newline
-
-
-# part 2
-input.seek(0)
-
 line = input.readline()[:-1] #chop off '\n'
 linelen = len(line)
 outlen = 0
 
-setrecursionlimit(999999999)
+i = 0
+while i < linelen:
+    if line[i] == '(':
+        j = i
+        while line[j] != ')':
+            j += 1
+        repLen, repCount = line[i+1:j].split('x')
+        repLen = int(repLen)
+        repCount = int(repCount)
+        addedLen = repLen * repCount 
+        outlen += addedLen
+        i = j + repLen + 1
+    else:
+        outlen += 1
+        i += 1
+
+print(outlen)
+
+
+# part 2
+
+outlen = 0
+
+setrecursionlimit(50)
 # recursively find length of a section of line
 def sectionLength(start, length):
     secLen = 0
     ii = start
     while ii < length + start:
-        # secLen += 1
-        # ii += 1
         if line[ii] == '(':
             jj = ii
             while line[jj] != ')':
