50626498095c6b808398d4f5794f1640007f3b24
iamevn <iamevn@users.noreply.github.com>
2016-12-20 02:03:29 -0500

    20 part 2. changed part 1 to work the same way as I get the answer to that for free here

--- 20/advent20.py
+++ 20/advent20.py
@@ -13,11 +13,47 @@ try:
 except OSError:
     exit("Cannot open file \"{}\"".format(input_filename))
 
-lowest_allowed = 0
+# list of tuples representing disjoint, inclusive, ranges
+allowed_ranges = [(0, 4294967295)]
+
+def split_ranges(allowed_ranges, rlow, rhigh):
+    new_ranges = []
+    for low, high in allowed_ranges:
+
+        # subrange starts in remove range
+        if rlow <= low <= rhigh < high:
+            new_ranges.append((rhigh + 1, high))
+
+        # subrange ends in remove range
+        elif low < rlow <= high <= rhigh:
+            new_ranges.append((low, rlow - 1))
+
+        # subrange contains range to remove
+        elif low < rlow <= rhigh < high:
+            new_ranges.append((low, rlow - 1))
+            new_ranges.append((rhigh + 1, high))
+
+        # subrange completely in range to remove 
+        elif rlow <= low <= high <= rhigh:
+            pass
+
+        # range to remove not in subrange
+        else:
+            new_ranges.append((low, high))
+
+    return new_ranges
+
 for line in input:
     low, high = map(int, line.split('-'))
-    if low <= lowest_allowed <= high:
-        lowest_allowed = high + 1
-        input.seek(0)
+    allowed_ranges = split_ranges(allowed_ranges, low, high)
 
-print(lowest_allowed)
+# part 1, lowest allowed
+print(allowed_ranges[0][0])
+
+# part 2, how many allowed
+count = 0
+
+for r in allowed_ranges:
+    count += r[1] - r[0] + 1
+
+print(count)
